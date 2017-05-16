0ea9f593ed2cd623d57828292a2bc12114ca6ab7
Felipe "Zoc" Silveira <TheZoc@users.github.com>
2017-05-16 02:19:03 -0300

    - Initial Commit - Added basic README.md - Added BSD-3 License - Added example screenshot - Added base code

---
+++ AutoTextType/AutoTextTyper.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace AutoTextType
+{
+    partial class frmAutoTextTyper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLP = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_SendText = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtProcessName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLP
+            // 
+            this.flowLP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLP.AutoScroll = true;
+            this.flowLP.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLP.Location = new System.Drawing.Point(12, 41);
+            this.flowLP.Name = "flowLP";
+            this.flowLP.Size = new System.Drawing.Size(916, 421);
+            this.flowLP.TabIndex = 0;
+            this.flowLP.WrapContents = false;
+            this.flowLP.SizeChanged += new System.EventHandler(this.FlowLPSizeChanged);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Add.Location = new System.Drawing.Point(853, 12);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 1;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_SendText
+            // 
+            this.btn_SendText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_SendText.Location = new System.Drawing.Point(853, 468);
+            this.btn_SendText.Name = "btn_SendText";
+            this.btn_SendText.Size = new System.Drawing.Size(75, 23);
+            this.btn_SendText.TabIndex = 4;
+            this.btn_SendText.Text = "Send Active";
+            this.btn_SendText.UseVisualStyleBackColor = true;
+            this.btn_SendText.Click += new System.EventHandler(this.btn_SendText_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 468);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(832, 82);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(769, 562);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 6;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // txtProcessName
+            // 
+            this.txtProcessName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtProcessName.Location = new System.Drawing.Point(115, 564);
+            this.txtProcessName.Name = "txtProcessName";
+            this.txtProcessName.Size = new System.Drawing.Size(648, 20);
+            this.txtProcessName.TabIndex = 7;
+            this.txtProcessName.Text = "Microsoft Visual Studio";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 567);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Target Application:";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(853, 497);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmAutoTextTyper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 596);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProcessName);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_SendText);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.flowLP);
+            this.Name = "frmAutoTextTyper";
+            this.Text = "Automatic Text Typer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLP;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_SendText;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox txtProcessName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ AutoTextType/AutoTextTyper.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace AutoTextType
+{
+    public partial class frmAutoTextTyper : Form
+    {
+        #region Imports
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        //this is a constant indicating the window that we want to send a text message
+        const int WM_SETTEXT = 0X000C;
+        #endregion
+
+        private bool CancelExecution = false;
+
+        public frmAutoTextTyper()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            TextBox newTextbox = new TextBox();
+            newTextbox.Multiline = true;
+            newTextbox.ScrollBars = ScrollBars.Vertical;
+            newTextbox.Font = new Font("Consolas", 10);
+            newTextbox.Width = flowLP.Width - 30;
+            newTextbox.Height = 100;
+
+            flowLP.Controls.Add(newTextbox);
+        }
+
+        private void FlowLPSizeChanged(object sender, EventArgs e)
+        {
+            foreach (TextBox tb in flowLP.Controls)
+            {
+                tb.Width = flowLP.Width - 30;
+            }
+        }
+
+        private void btn_SendText_Click(object sender, EventArgs e)
+        {
+            // Only proceed if we have a selected process.
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            // Get the target process and focus the main window
+            string[] split = listBox1.SelectedItem.ToString().Split(')');
+            int pid = Convert.ToInt32(split[0]);
+            Process p;
+            try
+            {
+                p = Process.GetProcessById(pid);
+            }
+            catch (ArgumentException exception)
+            {
+                btnRefresh_Click(this, null);
+                return;
+            }
+
+            // Bring our target window to foreground
+            SetForegroundWindow(p.MainWindowHandle);
+
+            // Prepare support variables and components
+            Random random = new Random();
+            CancelExecution = false;
+            btnCancel.Enabled = true;
+
+            foreach (TextBox tb in flowLP.Controls)
+            {
+                if (tb.Enabled == true)
+                {
+                    // Process it character by character
+                    string text = tb.Text;
+                    foreach (char c in text)
+                    {
+                        // Check if the cancel button was pressed
+                        if (CancelExecution)
+                            return;
+
+                        // Bring the target window foreground
+                        SetForegroundWindow(p.MainWindowHandle);
+
+                        // Replace the special keys and send the content
+                        string t = Regex.Replace(c.ToString(), "[+^%~(){}]", "{$0}");
+                        SendKeys.SendWait(t.ToString());
+
+                        // Sleep some time between key presses to simulate human behavior
+                        int SleepAmount = 100 + random.Next(0, 50);
+                        if (Char.IsWhiteSpace(c)) SleepAmount += 100 + random.Next(0, 75); // Sleep a bit more for spaces
+                        Thread.Sleep(SleepAmount);
+                    }
+
+                    // Disable the text box with the text that was just sent
+                    tb.Enabled = false;
+
+                    // Disable Cancel Button
+                    btnCancel.Enabled = false;
+                    break;
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            // Start empty
+            listBox1.Items.Clear();
+
+            // Get all the processes running
+            Process[] processes = Process.GetProcesses();
+
+            // Filter processes by name and add them to the list
+            foreach (Process p in processes)
+            {
+                if (p.MainWindowTitle.ToLower().Contains(txtProcessName.Text.ToLower()) && !p.MainWindowTitle.Trim().Equals(""))
+                {
+                    listBox1.Items.Add(p.Id + ") " + p.MainWindowTitle);
+                }
+            }
+
+            // If we found at least one process, select the first one
+            if (listBox1.Items.Count > 0)
+                listBox1.SetSelected(0, true);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Set the flag to cancel the execution and disable the button
+            CancelExecution = true;
+            btnCancel.Enabled = false;
+        }
+    }
+}
