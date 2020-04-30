e57c8b1ada6dc876525ac4c7c08a3e58836fb200
Arthur <Arthur@DESKTOP-RKANC1U>
2020-04-30 01:54:35 +0100

    Updates

--- AutoTextType/AutoTextTyper.Designer.cs
+++ AutoTextType/AutoTextTyper.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAutoTextTyper));
             this.flowLP = new System.Windows.Forms.FlowLayoutPanel();
             this.btn_Add = new System.Windows.Forms.Button();
             this.btn_SendText = new System.Windows.Forms.Button();
@@ -36,6 +38,10 @@
             this.txtProcessName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.setSpeed = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // flowLP
@@ -47,32 +53,35 @@
             this.flowLP.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLP.Location = new System.Drawing.Point(12, 41);
             this.flowLP.Name = "flowLP";
-            this.flowLP.Size = new System.Drawing.Size(916, 421);
+            this.flowLP.Size = new System.Drawing.Size(800, 421);
             this.flowLP.TabIndex = 0;
             this.flowLP.WrapContents = false;
             this.flowLP.SizeChanged += new System.EventHandler(this.FlowLPSizeChanged);
+            this.flowLP.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLP_Paint);
             // 
             // btn_Add
             // 
             this.btn_Add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_Add.Location = new System.Drawing.Point(853, 12);
+            this.btn_Add.Location = new System.Drawing.Point(737, 12);
             this.btn_Add.Name = "btn_Add";
             this.btn_Add.Size = new System.Drawing.Size(75, 23);
             this.btn_Add.TabIndex = 1;
             this.btn_Add.Text = "Add";
             this.btn_Add.UseVisualStyleBackColor = true;
-            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            this.btn_Add.Click += new System.EventHandler(this.Btn_Add_Click);
             // 
             // btn_SendText
             // 
             this.btn_SendText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_SendText.Location = new System.Drawing.Point(853, 468);
+            this.btn_SendText.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btn_SendText.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btn_SendText.Location = new System.Drawing.Point(737, 527);
             this.btn_SendText.Name = "btn_SendText";
             this.btn_SendText.Size = new System.Drawing.Size(75, 23);
             this.btn_SendText.TabIndex = 4;
             this.btn_SendText.Text = "Send Active";
-            this.btn_SendText.UseVisualStyleBackColor = true;
-            this.btn_SendText.Click += new System.EventHandler(this.btn_SendText_Click);
+            this.btn_SendText.UseVisualStyleBackColor = false;
+            this.btn_SendText.Click += new System.EventHandler(this.Btn_SendText_Click);
             // 
             // listBox1
             // 
@@ -81,19 +90,19 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(12, 468);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(832, 82);
+            this.listBox1.Size = new System.Drawing.Size(482, 82);
             this.listBox1.TabIndex = 5;
             // 
             // btnRefresh
             // 
             this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRefresh.Location = new System.Drawing.Point(769, 562);
+            this.btnRefresh.Location = new System.Drawing.Point(500, 562);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.Size = new System.Drawing.Size(102, 23);
             this.btnRefresh.TabIndex = 6;
-            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.Text = "Get or Refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
-            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
             // 
             // txtProcessName
             // 
@@ -101,9 +110,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtProcessName.Location = new System.Drawing.Point(115, 564);
             this.txtProcessName.Name = "txtProcessName";
-            this.txtProcessName.Size = new System.Drawing.Size(648, 20);
+            this.txtProcessName.Size = new System.Drawing.Size(379, 20);
             this.txtProcessName.TabIndex = 7;
-            this.txtProcessName.Text = "Microsoft Visual Studio";
+            this.txtProcessName.TextChanged += new System.EventHandler(this.TxtProcessName_TextChanged);
             // 
             // label2
             // 
@@ -118,20 +127,69 @@
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.btnCancel.Enabled = false;
-            this.btnCancel.Location = new System.Drawing.Point(853, 497);
+            this.btnCancel.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnCancel.Location = new System.Drawing.Point(737, 562);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 9;
             this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // setSpeed
+            // 
+            this.setSpeed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.setSpeed.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.setSpeed.Location = new System.Drawing.Point(737, 472);
+            this.setSpeed.Name = "setSpeed";
+            this.setSpeed.Size = new System.Drawing.Size(75, 23);
+            this.setSpeed.TabIndex = 10;
+            this.setSpeed.Text = "Set Speed";
+            this.setSpeed.UseVisualStyleBackColor = false;
+            this.setSpeed.Click += new System.EventHandler(this.setSpeed_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(631, 474);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "1";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(512, 477);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Set the Typing Speed:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Running";
+            this.notifyIcon1.BalloonTipTitle = "Restore by clicking here";
+            this.notifyIcon1.Text = "AutoText";
+            this.notifyIcon1.Visible = true;
             // 
             // frmAutoTextTyper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(940, 596);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(824, 596);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.setSpeed);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtProcessName);
@@ -140,8 +198,11 @@
             this.Controls.Add(this.btn_SendText);
             this.Controls.Add(this.btn_Add);
             this.Controls.Add(this.flowLP);
+            this.ForeColor = System.Drawing.Color.DarkBlue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmAutoTextTyper";
             this.Text = "Automatic Text Typer";
+            this.Load += new System.EventHandler(this.frmAutoTextTyper_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,6 +218,10 @@
         private System.Windows.Forms.TextBox txtProcessName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button setSpeed;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- AutoTextType/AutoTextTyper.cs
+++ AutoTextType/AutoTextTyper.cs
@@ -9,23 +9,57 @@ using System.Threading;
 namespace AutoTextType
 {
     public partial class frmAutoTextTyper : Form
+
     {
         #region Imports
         [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hWnd, uint Msg);
+        #endregion
+
+        private int sleepAmount;
+        private int value;
+        private int speed;
 
         //this is a constant indicating the window that we want to send a text message
         const int WM_SETTEXT = 0X000C;
-        #endregion
+        //this is a constant indicating the window that we want to restore from a minimized state
+        private const uint SW_RESTORE = 0x09;
 
         private bool CancelExecution = false;
+        private int sleep;
 
         public frmAutoTextTyper()
         {
             InitializeComponent();
+            
+            // When window state changed, trigger state update.
+            this.Resize += SetMinimizeState;
+
+            // When tray icon clicked, trigger window state change.       
+            notifyIcon1.Click += ToggleMinimizeState;
+        }
+        // Toggle state between Normal and Minimized.
+        private void ToggleMinimizeState(object sender, EventArgs e)
+        {
+            bool isMinimized = this.WindowState == FormWindowState.Minimized;
+            this.WindowState = (isMinimized) ? FormWindowState.Normal : FormWindowState.Minimized;
         }
 
-        private void btn_Add_Click(object sender, EventArgs e)
+        // Show/Hide window and tray icon to match window state.
+        private void SetMinimizeState(object sender, EventArgs e)
+        {
+            bool isMinimized = this.WindowState == FormWindowState.Minimized;
+
+            this.ShowInTaskbar = !isMinimized;
+            notifyIcon1.Visible = isMinimized;
+            if (isMinimized) notifyIcon1.ShowBalloonTip(500, "Auto Text", "Application minimized to tray.", ToolTipIcon.Info);
+        }
+
+        private void Btn_Add_Click(object sender, EventArgs e)
         {
             TextBox newTextbox = new TextBox();
             newTextbox.Multiline = true;
@@ -45,7 +79,7 @@ namespace AutoTextType
             }
         }
 
-        private void btn_SendText_Click(object sender, EventArgs e)
+        private void Btn_SendText_Click(object sender, EventArgs e)
         {
             // Only proceed if we have a selected process.
             if (listBox1.SelectedIndex == -1)
@@ -61,10 +95,12 @@ namespace AutoTextType
             }
             catch (ArgumentException exception)
             {
-                btnRefresh_Click(this, null);
+                BtnRefresh_Click(this, null);
                 return;
             }
-
+            
+            //Bring our target if is minimized
+            ShowWindow(p.MainWindowHandle, SW_RESTORE);
             // Bring our target window to foreground
             SetForegroundWindow(p.MainWindowHandle);
 
@@ -79,23 +115,50 @@ namespace AutoTextType
                 {
                     // Process it character by character
                     string text = tb.Text;
+                    
                     foreach (char c in text)
                     {
                         // Check if the cancel button was pressed
                         if (CancelExecution)
                             return;
 
-                        // Bring the target window foreground
-                        SetForegroundWindow(p.MainWindowHandle);
-
                         // Replace the special keys and send the content
                         string t = Regex.Replace(c.ToString(), "[+^%~(){}]", "{$0}");
                         SendKeys.SendWait(t.ToString());
 
                         // Sleep some time between key presses to simulate human behavior
-                        int SleepAmount = 100 + random.Next(0, 50);
-                        if (Char.IsWhiteSpace(c)) SleepAmount += 100 + random.Next(0, 75); // Sleep a bit more for spaces
-                        Thread.Sleep(SleepAmount);
+                        sleep = 100 + random.Next(0, 50);
+                        if (Char.IsWhiteSpace(c)) sleep += 100 + random.Next(0, 75); // Sleep a bit more for spaces
+                        sleepAmount = sleep - speed;
+                        Thread.Sleep(sleepAmount);
+
+                        //check if the target Pid is in foreground or minimized
+                        var activatedHandle = GetForegroundWindow();
+                        if (activatedHandle != p.MainWindowHandle)
+                        {
+                            DialogResult result1 = MessageBox.Show("Your main window is not selected, do you want to continue typing?",
+                                "Interruption", MessageBoxButtons.YesNo);
+                            if (result1 == DialogResult.Yes)
+                            {
+                                //Bring our target if is minimized
+                                ShowWindow(p.MainWindowHandle, SW_RESTORE);
+                                // Bring back the target window foreground
+                                SetForegroundWindow(p.MainWindowHandle);
+                                if (p.HasExited)
+                                {
+                                    MessageBox.Show("The targe application was closed", "Ops...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    CancelExecution = true;
+                                    btnCancel.Enabled = false;
+                                }
+                                continue;
+                            }
+                            else
+                            {
+                                CancelExecution = true;
+                                btnCancel.Enabled = false;
+                                break;
+                            } 
+                        }
                     }
 
                     // Disable the text box with the text that was just sent
@@ -108,7 +171,7 @@ namespace AutoTextType
             }
         }
 
-        private void btnRefresh_Click(object sender, EventArgs e)
+        private void BtnRefresh_Click(object sender, EventArgs e)
         {
             // Start empty
             listBox1.Items.Clear();
@@ -130,11 +193,44 @@ namespace AutoTextType
                 listBox1.SetSelected(0, true);
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
+        private void BtnCancel_Click(object sender, EventArgs e)
         {
             // Set the flag to cancel the execution and disable the button
             CancelExecution = true;
             btnCancel.Enabled = false;
+        }
+
+        private void TxtProcessName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            bool success = Int32.TryParse(textBox1.Text, out value);
+            if (success == true)
+            {
+                speed = value;
+            }
+            else
+            {
+                MessageBox.Show("Only numbers 1 to 1000", "Ops...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void setSpeed_Click(object sender, EventArgs e)
+        {
+            sleepAmount = speed;
+        }
+
+        private void flowLP_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmAutoTextTyper_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
